1e985e8ff9bf30febbb76b186e5a18b198f79f08
Rasi Shil <rasishil26@gmail.com>
2025-06-23 14:54:21 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,20 +1,54 @@
 # Rock Paper Scissors Game
 
 import random
+rock = '''
+    _______
+---'   ____)
+      (_____)
+      (_____)
+      (____)
+---.__(___)
+'''
 
-options = ["Rock", "Paper", "Scissors"]
+paper = '''
+    _______
+---'   ____)____
+          ______)
+          _______)
+         _______)
+---.__________)
+'''
 
-user_input = int(input("What do you choose? Type 0 for Rock, 1 for Paper or 2 for Scissors.\n"))
-if user_input < 0 or user_input > 2:
-    print("Invalid input. You lose!")
-else:
-    print(f"You chose: {options[user_input]}")
-    computer_choice = random.randint(0, 2)
-    print(f"Computer chose: {options[computer_choice]}")
+scissors = '''
+    _______
+---'   ____)____
+          ______)
+       __________)
+      (____)
+---.__(___)
+'''
 
-    if user_input == computer_choice:
-        print("It's a draw!")
-    elif (user_input - computer_choice) % 3 == 1:
-        print("You win! 🎉")
-    else:
-        print("You lose 😢")
+game_images =[rock, paper, scissors]
+
+user_choice = int(input("What do you choose? Type 0 for Rock, 1 for Paper or 2 for Scissors.\n"))
+
+computer_choice = random.randint(0, 2)
+if user_choice >= 0 and computer_choice <= 2:
+    print(game_images[user_choice])
+
+print("computer_choice:")
+print(game_images[computer_choice])
+
+if user_choice >= 3 or user_choice < 0:
+    print("You've typed invalid number. You lose!")
+elif user_choice == 0 and computer_choice == 2:
+    print("You win!")
+elif computer_choice == 0 and user_choice == 2:
+    print("You lose!")
+elif computer_choice > user_choice:
+    print("You lose!")
+elif user_choice > computer_choice:
+    print("You win!")
+elif computer_choice == user_choice:
+    print("It's a draw!")
+
